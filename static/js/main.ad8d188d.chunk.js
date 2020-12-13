fa1dbbebab4dd4375e5169749aaf23db1f36255c
rostislav228 <rostikpershin1@gmail.com>
2020-12-13 21:23:54 +0300

    Updates

--- static/js/main.ad8d188d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){},4:function(e,t,c){"use strict";c.r(t);var n=c(2),o=c(0),a=c.n(o),r=c(3),i=c.n(r);c(10);i.a.render(Object(n.jsx)(a.a.StrictMode,{}),document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.ad8d188d.chunk.js.map
